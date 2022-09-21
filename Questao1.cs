f613ed166f4e4a4ca7891dd134d0ee9111dc7c3f
Henrique Silva <steniohenriquelacerda@gmail.com>
2022-09-20 22:32:22 -0300

    Add files via upload

---
+++ Questao1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		try
+		{
+
+			Console.WriteLine("PA é: " + Funcao_PA(50, 10, 10));
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.Message);
+		}
+	}
+
+	public static int Funcao_PA(int ElementoInicial, int Razao, int Nesimo)
+	{
+
+		if (Nesimo < 0)
+		{
+			throw new InvalidOperationException("[ERRO] Nesimo não pode ser negativo");
+		}
+
+		int PA = ElementoInicial;
+
+		for (int i = 0; i < Nesimo; i++)
+		{
+			PA += Razao;
+		}
+
+		return PA;
+	}
+}
